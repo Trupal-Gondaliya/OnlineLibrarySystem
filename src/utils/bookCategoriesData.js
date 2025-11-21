46b432f83aefa78f6477e567088396bb9c9ab0f7
Trupal-Gondaliya <trupalgondaliya@gmail.com>
2025-11-21 16:45:37 +0530

    add book category data

---
+++ src/utils/bookCategoriesData.js
@@ -0,0 +1,27 @@
+import ScienceFiction from "../assets/ScienceFiction.jpeg";
+import Romance from "../assets/Romance.jpeg";
+import MysteryThriller from "../assets/MysteryThriller.jpeg";
+import HistoricalFiction from "../assets/HistoricalFiction.jpeg";
+import Horror from "../assets/Horror.jpeg";
+import Travel from "../assets/Travel.jpeg";
+import Biography from "../assets/Biography.jpeg";
+import SelfHelp from "../assets/SelfHelpPersonalDevelopment.jpeg";
+import Poetry from "../assets/Poetry.jpeg";
+import Business from "../assets/Business.jpeg";
+import Politics from "../assets/Politics.png";
+
+const bookCategoriesData = [
+    { id: 1, title: "Science Fiction", image: ScienceFiction },
+    { id: 2, title: "Romance", image: Romance },
+    { id: 3, title: "Mystery & Thriller", image: MysteryThriller },
+    { id: 4, title: "Historical Fiction", image: HistoricalFiction },
+    { id: 5, title: "Horror", image: Horror },
+    { id: 6, title: "Travel", image: Travel },
+    { id: 7, title: "Biography", image: Biography },
+    { id: 8, title: "Self Help & Personal Development", image: SelfHelp },
+    { id: 9, title: "Poetry", image: Poetry },
+    { id: 10, title: "Business", image: Business },
+    { id: 11, title: "Politics", image: Politics },
+];
+
+export default bookCategoriesData;
